ea2e8795ef3591444dce0d2ed5e8e013cc61aa73
loatheb <zz.innj@gmail.com>
2017-10-16 23:53:38 +0800

    feat(callback): add main content about the tiny-callback

---
+++ tiny-callbacks/index.js
@@ -0,0 +1,41 @@
+const Callbacks = (flags) => {
+  const flagsList = flags.split(' ')
+  let fired
+  class Callbacks {
+    constructor() {
+      this.callbacks = []
+      this.callbackObject = {}
+      this.memoryArgs = []
+
+      this.once = flagsList.includes('once')
+      this.unique = flagsList.includes('unique')
+      this.stopOnFalse = flagsList.includes('stopOnFalse')
+      this.memory = flagsList.includes('memory')
+    }
+    add(fn) {
+      const shouldAddFn = this.unique && this.callbackObject[fn] || !this.unique
+      if (shouldAddFn) {
+        this.callbacks.push(fn)
+        this.callbackObject[fn] = true
+      }
+      if (this.memory) {
+        this.memoryArgs.every(args => {
+          const status = fn.apply(fn, args)
+          return !this.stopOnFalse || !status
+        })
+      }
+    }
+    fire(...args) {
+      if (this.once && fired) return this
+      this.memoryArgs.push(args)
+
+      this.callbacks.every(cb => {
+        const status = cb.apply(cb, args)
+        return !this.stopOnFalse || !status
+      })
+      fired = true
+    }
+  }
+
+  return new Callbacks()
+}
---
+++ tiny-callbacks/readme.md
@@ -0,0 +1,9 @@
+精简版 jQuery callback 函数，通过传入的 flag 产生不同效果
+
+'once' fire 函数只触发一次
+
+'memory' 缓存上一次执行的结果，并且在下一次 fire 时继续执行
+
+'unique' 所有 add 的函数只存在一个副本
+
+'stopOnFalse' 当一个函数返回 false 时停止继续触发
