1cae571acc15074a9ab3807488ef16803839e938
zhao.zhang <zhao@strikingly.com>
2018-01-17 16:00:03 +0800

    feat(ast): add ast structure with the simple test

---
+++ infrastructure/ast/index.js
@@ -0,0 +1,14 @@
+class Parser {
+  constructor(code) {
+
+  }
+}
+
+module.exports = {
+  parseScript(code) {
+    return new Parser(code)
+  },
+  parseModule(code) {
+    /* will do... */
+  }
+}
---
+++ infrastructure/ast/test/index.js
@@ -0,0 +1,33 @@
+const assert = require('assert')
+const { parseScript } = require('..')
+
+/* pattern with the ; */
+const ast1 = parseScript('const foo = 123;')
+
+/* pattern without the ; */
+const ast2 = parseScript('const foo = 123')
+
+const ESTREE_AST = {
+  type: 'Program',
+  body: [{
+    type: 'VariableDeclaration',
+    declarations: [{
+      type: 'VariableDeclarator',
+      init: {
+        type: 'Literal',
+        value: 123
+      },
+      id: {
+        type: 'Identifier',
+        name: 'foo'
+      }
+      }],
+    kind: 'const'
+  }],
+  sourceType: 'script'
+}
+
+assert.deepEqual(ast1, ESTREE_AST)
+assert.deepEqual(ast1, ast2)
+
+console.log('all test passed')
