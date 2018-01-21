d31a1eab3caf6de8214f67ccfb8ef586bd151805
loatheb <loatheb.zhao@gmail.com>
2018-01-21 16:28:29 +0800

    feat(npm-install): add install command main file, now it will do 4 lifecycle to install a simple module under node_modules

---
+++ infrastructure/package-manager/utils/install.js
@@ -0,0 +1,58 @@
+/**
+ * Lifecycle: install --> installHelper --> parser --> parserHelper
+ * Because of this is a non based package so we don't use promise or fetch here.
+ * 
+ * INSTALL: generate the fetch url and return the request with the callback
+ * INSTALL_HELPER: get the package information and resolve the real url to fetch it.
+ * PARSER: parse the fetched data from .taz to normal
+ * PARSER_HELPER: write down to fake_node_modules
+ */
+
+const fs = require('fs')
+const path = require('path')
+const childProcess = require('child_process')
+const debug = require('./debug')('tnpm')
+const { get } = require('./request')
+
+const registry = 'https://registry.npm.taobao.org/'
+const modulesPath = 'node_modules'
+
+function install(package) {
+  const url = `${registry}${package}/`
+
+  debug('url', url)
+  return get(url, installHelper)
+}
+
+function installHelper(res) {
+  const { 'dist-tags': { latest: v } } = res
+  debug('version', v)
+
+  const { [v]: result } = res.versions
+  const { name, version, dist } = result
+  const { tarball } = dist
+
+  debug('name', name, '\nversion', version, '\ntarball', tarball)
+  console.log(`Installing ${name}@${version} ...`)
+  return get(tarball, parser(name))
+}
+
+function parser(name) {
+  if (!fs.existsSync(modulesPath)) {
+    fs.mkdirSync(modulesPath)
+  }
+  return function(tgzName) {
+    return childProcess.exec(`tar -xf ./${tgzName} -C ${modulesPath}`, (error, stdout, stderr) => {
+      if (error) return console.error(`exec error: ${error}`)
+      if (stdout) console.log(`stdout: ${stdout}`)
+      if (stderr) console.log(`stderr: ${stderr}`)
+      return parserHelper(name, tgzName)
+    })
+  }
+}
+
+function parserHelper(name, tgzName) {
+  return childProcess.exec(`mv ${modulesPath}/package ${modulesPath}/${name} && rm ${tgzName}`)
+}
+
+module.exports = install
