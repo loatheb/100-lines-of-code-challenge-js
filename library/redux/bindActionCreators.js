622834c091361413d7dffbdd97dda641616629b9
loatheb <loatheb.zhao@gmail.com>
2018-01-23 23:09:47 +0800

    refactor(bindActionCreator): refactor the action creator to export just one method

--- library/redux/bindActionCreators.js
+++ library/redux/bindActionCreators.js
@@ -1,11 +1,3 @@
-var bindActionCreator = function (actionCreator, dispatch) {
-  return function () {
-    var args = [].slice.call(arguments)
-    return dispatch(actionCreator(args))
-  }
-}
-
-
-function bindActionCreator(actionCreator, dispatch) {
+module.exports = function bindActionCreator(actionCreator, dispatch) {
   return (...args) => dispatch(actionCreator(...args))
 }
