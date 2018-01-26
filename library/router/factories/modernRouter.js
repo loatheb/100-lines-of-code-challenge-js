f273467f00c1be9170db5eff78f5e0343c884764
loatheb <loatheb.zhao@gmail.com>
2018-01-26 20:26:16 +0800

    refactor(mordenRouter): update the mordenRouter to trigger the router callback at the first time

--- library/router/factories/modernRouter.js
+++ library/router/factories/modernRouter.js
@@ -1,11 +1,20 @@
+const { isFunction } = require('./utils')
+
 module.exports = function router(map) {
-  const links = [...document.getElementsByTagName('link')]
+  const links = [...document.getElementsByTagName('a')]
+  const currentRoute = location.pathname
+  const method = map[currentRoute]
+
+  if (method && isFunction(method)) {
+    method()
+  }
+
   links.forEach(link => {
     const target = link.getAttribute('target')
     link.onclick = function() {
       const method = map[target]
       history.pushState({}, target, target)
-      if (method && typeof method === 'function') return method()
+      if (method && isFunction(method)) return method()
     }
   })
 }
