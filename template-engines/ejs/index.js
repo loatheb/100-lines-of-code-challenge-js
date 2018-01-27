abe4180e8d87435ec27c9b3a8cff40066a3e0130
loatheb <loatheb.zhao@gmail.com>
2018-01-27 15:55:48 +0800

    feat(ejs/index): started build main structure parse compile render in index

--- template-engines/ejs/index.js
+++ template-engines/ejs/index.js
@@ -1,13 +1,30 @@
-function compile() {
-
+function parse(str) {
+  return `
+    var buf = [];
+    with(data) {
+      buf.push('${
+        str
+          .replace(/[\r\t\n]/g, " ")
+          .split("<%").join("\t")
+          .replace(/((^|%>)[^\t]*)'/g, "$1\r")
+          .split("'").join("\\'")
+          .replace(/\t=(.*?)%>/g, "',$1,'")
+          .split("\t").join("');")
+          .split("%>").join("buf.push('")
+          .split("\r").join("\\'")
+      }');
+    };
+    return buf.join('');
+  `
 }
 
-function parse() {
-
+function compile(str) {
+  return new Function('data', parse(str))
 }
 
 function render(str, data) {
-
+  const fn = compile(str)
+  return fn.call(null, data)
 }
 
 module.exports = {
