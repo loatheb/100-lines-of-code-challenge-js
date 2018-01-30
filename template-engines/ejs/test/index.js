e08e8e45953ec6b61b0252a310eb147b08e7b5ba
loatheb <loatheb.zhao@gmail.com>
2018-01-30 17:45:27 +0800

    fix(ejs/test): use jest describe method to put the special test case in their own scope

--- template-engines/ejs/test/index.js
+++ template-engines/ejs/test/index.js
@@ -1,34 +1,36 @@
-const tejs = require('..')
+const { render } = require('..')
 
 function format(str) { return str.replace(/[\r\t\n\s]/g, '') }
 
-test('render simple html snippet', () => {
-  const template = '<% if (user) { %><h2><%= user.name %></h2><% } %>'
-  const data = {
-    user: { name: 'zhangzhao' },
-  }
+describe('[ejs]: test for the render scope', () => {
+  test('render simple html snippet', () => {
+    const template = '<% if (user) { %><h2><%= user.name %></h2><% } %>'
+    const data = {
+      user: { name: 'zhangzhao' },
+    }
 
-  const result = tejs.render(template, data)
-  expect(format(result)).toBe('<h2>zhangzhao</h2>')
-})
+    const result = render(template, data)
+    expect(format(result)).toBe('<h2>zhangzhao</h2>')
+  })
 
-test('render complex html snippet with array forEach loop', () => {
-  const template = `
-    <ul>
-      <% users.forEach(function(user){ %>
-        <% if (user) { %>
-          <h2><%= user.name %></h2>
-        <% } %>
-      <% }); %>
-    </ul>
-  `
-  const data = {
-    users: [
-      { name: 'zhao.zhang' },
-      { name: 'lingli.chen' },
-    ],
-  }
+  test('render complex html snippet with array forEach loop', () => {
+    const template = `
+      <ul>
+        <% users.forEach(function(user){ %>
+          <% if (user) { %>
+            <h2><%= user.name %></h2>
+          <% } %>
+        <% }); %>
+      </ul>
+    `
+    const data = {
+      users: [
+        { name: 'zhao.zhang' },
+        { name: 'lingli.chen' },
+      ],
+    }
 
-  const result = tejs.render(template, data)
-  expect(format(result)).toBe('<ul><h2>zhao.zhang</h2><h2>lingli.chen</h2></ul>')
+    const result = render(template, data)
+    expect(format(result)).toBe('<ul><h2>zhao.zhang</h2><h2>lingli.chen</h2></ul>')
+  })
 })
